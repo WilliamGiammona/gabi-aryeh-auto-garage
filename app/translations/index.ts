61cb08542de7e850f84f89f9b44f55d66acdd4b6
William Giammona <wgiammona@gmail.com>
2025-05-12 19:53:47 +0300

    initial commit

---
+++ app/translations/index.ts
@@ -0,0 +1,21 @@
+// app/translations/index.ts
+export const translations = {
+  en: {
+    siteName: "Gabi & Aryeh's Auto Garage",
+    home: "Home",
+    about: "About",
+    services: "Services",
+    contact: "Contact",
+    jobs: "Jobs",
+    switchToHebrew: "עברית",
+  },
+  he: {
+    siteName: "המוסך של גבי ואריה",
+    home: "בית",
+    about: "אודות",
+    services: "שירותים",
+    contact: "צור קשר",
+    jobs: "דרושים",
+    switchToEnglish: "English",
+  },
+};
